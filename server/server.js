e80e4dad387a7bd4624fdf59e458a0b373378eb7
lmohade3l <mohade3.2710@gmail.com>
2025-06-27 00:52:29 +0330

    feat: update server.js

--- server/server.js
+++ server/server.js
@@ -7,3 +7,54 @@ const { renderToPipeableStream } = require("react-server-dom-webpack/server");
 const AppImport = require("../src/App.jsx");
 
 const App = AppImport.default;
+
+const MANIFEST = readFileSync(
+  path.resolve(__dirname, "../dist/react-client-manifest.json") , "utf8"
+);
+
+const MODULE_MAP = JSON.parse(MANIFEST); // this is now ready to be passed to the webpack plugin
+const PORT = process.env.PORT ? process.env.PORT : 3000;
+
+const fastify = Fastify({
+  logger: {
+    transport: {
+      target: "pino-pretty",
+    },
+  },
+});
+
+fastify.register(fastifyStaticPlugin, {
+  root: path.join(__dirname, "../dist"),
+  prefix: "/assets/",
+});
+
+fastify.register(fastifyStaticPlugin, {
+  root: path.join(__dirname, "../public"),
+  decorateReply: false,
+});
+
+fastify.get("/", async function rootHandlre(request, reply) {
+  return reply.sendFile("index.html");
+});
+
+fastify.get("/react-flight", function reactFlightHandler(request, reply) {
+  try {
+    reply.header("Content-Type", "application/octet-stream");
+return reply.send(`1:{"name":"App","env":"Server","key":null,"owner":null,"props":{}}
+0:D"$1"
+0:["$","div",null,{"children":["$","h1",null,{"children":"Notes App"},"$1"]},"$1"]
+`);
+  } catch (error) {
+    request.log.error("react-flight error", error);
+    throw error;
+  }
+});
+
+module.exports = async function start() {
+  try {
+    await fastify.listen({ port: PORT });
+  } catch (error) {
+    fastify.log.error(error);
+    process.exit(1);
+  }
+};
--- src/Client.jsx
+++ src/Client.jsx
@@ -1,11 +1,11 @@
-import { createRoot } from "react/client";
+import { createRoot } from "react-dom/client";
 import "doodle.css/doodle.css";
 
 // it's going to receive markup form the server and then it's gonna turn that markup in react component
 import { createFromFetch } from "react-server-dom-webpack/client";
 
 console.log("fetching flight response");
-const fetchPromise = fetch("react/flight");
+const fetchPromise = fetch("/react-flight");
 const root = createRoot(document.getElementById("root"));
 const p = createFromFetch(fetchPromise);
 console.log("rendering root", p);
