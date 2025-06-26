7bdb864f80ab128f4f45ce63cfe19281baeda552
lmohade3l <mohade3.2710@gmail.com>
2025-06-26 23:18:51 +0330

    feat: add the client component

---
+++ src/Client.jsx
@@ -0,0 +1,14 @@
+import { createRoot } from "react/client";
+import "doodle.css/doodle.css";
+
+// it's going to receive markup form the server and then it's gonna turn that markup in react component
+import { createFromFetch } from "react-server-dom-webpack/client";
+
+console.log("fetching flight response");
+const fetchPromise = fetch("react/flight");
+const root = createRoot(document.getElementById("root"));
+const p = createFromFetch(fetchPromise);
+console.log("rendering root", p);
+
+// this goes and gets the markup from the server and render's gonna await on that promise to be done and then it'll be rendered
+root.render(p);
